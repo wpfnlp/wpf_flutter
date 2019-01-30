dff0c2074a8f2e86273ea18b5ca067d271282007
weipengfei <857924849@qq.com>
2019-01-30 13:19:40 +0800

    material_components input dialog commit

--- lib/demo/material_component.dart
+++ lib/demo/material_component.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+
+import 'alert_dialog_demo.dart';
+import 'bottom_sheet_demo.dart';
 import 'button_demo.dart';
+import 'checkbox_demo.dart';
+import 'datetime_demo.dart';
+import 'expansion_panel_demo.dart';
 import 'floating_action_demo.dart';
+import 'form_demo.dart';
 import 'popup_menu_button_demo.dart';
+import 'radio_demo.dart';
+import 'simple_dialog_demo.dart';
+import 'slider_demo.dart';
+import 'snack_bar_demo.dart';
+import 'switch_demo.dart';
 
 class MaterialComponents extends StatelessWidget {
   @override
@@ -13,6 +25,17 @@ class MaterialComponents extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          ListItem(title: 'ExpansionPanelDemo', page: ExpansionPanelDemo()),
+          ListItem(title: 'SnackBarDemo', page: SnackBarDemo()),
+          ListItem(title: 'AlertDialogDemo', page: AlertDialogDemo()),
+          ListItem(title: 'BottomSheetDemo', page: BottomSheetDemo()),
+          ListItem(title: 'SimpleDialogDemo', page: SimpleDialogDemo()),
+          ListItem(title: 'DateTimeDemo', page: DateTimeDemo()),
+          ListItem(title: 'SliderDemo', page: SliderDemo()),
+          ListItem(title: 'SwitchDemo', page: SwitchDemo()),
+          ListItem(title: 'RadioDemo', page: RadioDemo()),
+          ListItem(title: 'CheckboxDemo', page: CheckboxDemo()),
+          ListItem(title: 'FormDemo', page: FormDemo()),
           ListItem(title: 'PopupMenuButton', page: PopupMenuButtonDemo()),
           ListItem(title: 'Button', page: ButtonDemo()),
           ListItem(
